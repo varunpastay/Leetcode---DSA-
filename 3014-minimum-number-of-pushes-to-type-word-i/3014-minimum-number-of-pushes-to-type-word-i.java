830d99d07a3f54166bb09f0868a06143a706769d
Varun Pastay <128835437+varunpastay@users.noreply.github.com>
2026-07-30 10:25:30 +0530

    Time: 0 ms (100%), Space: 43.3 MB (47.13%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int pushes=0;
+        for(int i=0;i<word.length();i++){
+            pushes=pushes+(i/8)+1;
+        }
+        return pushes;
+    }
+}
