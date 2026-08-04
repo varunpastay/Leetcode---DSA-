e6bcb83ef082769854b1ffe7e6d9652b0c5ca770
Varun Pastay <128835437+varunpastay@users.noreply.github.com>
2026-08-04 22:09:20 +0530

    Time: 4 ms (29.33%), Space: 46.3 MB (61.43%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,14 +1,11 @@
 class Solution {
     public String intToRoman(int num) {
-        // Map values and symbols in descending order
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         
         StringBuilder roman = new StringBuilder();
         
-        // Loop through the values array
         for (int i = 0; i < values.length; i++) {
-            // Repeat the symbol while the number is greater than or equal to the current value
             while (num >= values[i]) {
                 roman.append(symbols[i]);
                 num -= values[i];
