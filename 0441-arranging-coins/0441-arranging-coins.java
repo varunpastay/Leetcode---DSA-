3baca3c71fd0fc4e324773f5527a6094014f79e0
Varun Pastay <128835437+varunpastay@users.noreply.github.com>
2026-08-20 10:56:34 +0530

    Time: 5 ms (44.13%), Space: 42.8 MB (25.12%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int row=1;
+        while(n>=row){
+            n=n-row;
+            row++;
+        }
+        return row-1;
+        
+    }
+}
