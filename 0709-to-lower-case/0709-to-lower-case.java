7dd7ac573dd3d9dd023f4b611ea4f6944a873470
Varun Pastay <128835437+varunpastay@users.noreply.github.com>
2026-08-28 15:42:44 +0530

    Time: 3 ms (10.14%), Space: 43.8 MB (5.25%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+
+            result = result + ch;
+        }
+
+        return result;
+    }
+}
