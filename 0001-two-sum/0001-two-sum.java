7edd970d2b65ab3abfe1a40d2db71e6c720b790c
Varun Pastay <128835437+varunpastay@users.noreply.github.com>
2026-09-04 09:33:50 +0530

    Time: 49 ms (8.14%), Space: 46.8 MB (85.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,52 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int sum=0;
+        int[] res=new int[2];
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
-                sum=nums[i]+nums[j];
-            if(sum==target){
-                return new int[]{i,j};
-            }
-
+                int sum=nums[i]+nums[j];
+                if(sum==target){
+                    res[0]=i;
+                    res[1]=j;
+                }
             }
         }
-        return new int[]{};
+        return res;        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int sum=0;
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 sum=nums[i]+nums[j];
+//             if(sum==target){
+//                 return new int[]{i,j};
+//             }
+
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
